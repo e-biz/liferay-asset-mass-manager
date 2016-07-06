92bc2d832def14478e12c656a3d750ed1120fdb5
nbelleme <nbelleme@excilys.com>
2016-07-06 10:48:00 +0200

    [View | JS] add js

--- docroot/js/main.js
+++ docroot/js/main.js
@@ -1,13 +1,47 @@
 (function($) {
+	var namespace = $("#portlet-namespace").data("namespace");
+	
 	$("#assets-column h2").on("click", function() {
 		var ul = $(this).parent().children("ul");
 		var isUlVisible = ul.is(":visible");
-		if(isUlVisible){	
+		if (isUlVisible) {
 			$(this).parent().children("ul").hide();
 			$(this).parent().children("ul").addClass("hidden");
-		}else{
+		} else {
 			$(this).parent().children("ul").show();
 			$(this).parent().children("ul").removeClass("hidden");
-		}	
+		}
 	});
+
+	$("#submitFormAssets").on("click", function(event) {
+		event.preventDefault();
+
+		var hiddenAssetId = "#"+namespace+"hiddenAssetId";
+		var hiddenTagId = "#"+namespace+"hiddenTagId";
+		var hiddenCategoryId = "#"+namespace+"hiddenCategoryId";
+
+		addToHidden("#assets-column", hiddenAssetId);
+		addToHidden("#tags-box", hiddenTagId);
+		addToHidden("#categories-box", hiddenCategoryId);
+		
+		console.log($(hiddenAssetId).val());
+		console.log($(hiddenTagId).val());
+		console.log($(hiddenCategoryId).val());
+		
+		// $("form :submit").trigger("click");
+
+	})
 })(jQuery);
+
+function submitForm(event) {
+
+}
+
+function addToHidden(fieldId, hiddenId) {
+	var assetCheckbox = $(fieldId + " input:checked");
+	var assetIds = Array();
+	for (var i = 0; i < assetCheckbox.length; i++) {
+		assetIds.push(assetCheckbox[i].value);
+	}
+	$(hiddenId).val(assetIds);
+}
